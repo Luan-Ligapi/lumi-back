5b5c0be1fe4c0915cfdde505c217ff54d93fbd35
Luan-Ligapi <luan@ligapi.com.br>
2024-10-19 15:28:30 -0300

    correção do upload
    ignore token

--- models/cliente.js
+++ models/cliente.js
@@ -1,20 +1,14 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Cliente = sequelize.define('Cliente', {
-    nome: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    },
-    numeroCliente: {  // Adicione este campo
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    }
+    nome: DataTypes.STRING,
+    email: DataTypes.STRING,
+    numeroCliente: DataTypes.STRING,
+    cpf_cnpj: DataTypes.STRING,
+    endereco: DataTypes.STRING,
+    telefone: DataTypes.STRING,
+    inscricao_estadual: DataTypes.STRING,
+    classe_consumidor: DataTypes.STRING
   }, {
     schema: 'lumi', // Define o schema onde a tabela será criada
   });
--- routes/upload.js
+++ routes/upload.js
@@ -1,50 +1,102 @@
-const express = require('express');
-const multer = require('multer');
-const pdfParser = require('../utils/pdfParser');
+const { Storage } = require('@google-cloud/storage');
 const path = require('path');
+const express = require('express');
 const router = express.Router();
+const pdfParser = require('../services/pdfParser');
+const { Fatura, Cliente } = require('../models');
+const multer = require('multer');
+const fs = require('fs');
 
+// Configuração do Google Cloud Storage
+const bucket = new Storage({
+  keyFilename: path.join(__dirname, '../config/gcp-key.json'),
+  projectId: 'noted-feat-437112-m5',
+});
+const bucketName = 'lumi-luan';
+
+// Verifique se o diretório 'uploads/' existe, se não, cria-o
+const ensureUploadsDir = () => {
+  const uploadsDir = path.join(__dirname, '../uploads');
+  if (!fs.existsSync(uploadsDir)) {
+    fs.mkdirSync(uploadsDir);
+  }
+};
+
+// Configurar o multer para armazenar o arquivo em uma pasta temporária
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'upload/');
-    },
-    filename: function (req, file, cb) {
-        cb(null, `${Date.now()}-${file.originalname}`);
-    }
+  destination: function (req, file, cb) {
+    ensureUploadsDir(); // Verificar e criar o diretório antes de armazenar o arquivo
+    cb(null, 'uploads/'); // Pasta onde os arquivos serão armazenados temporariamente
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  }
 });
 
-const upload = multer({ 
-    storage: storage,
-    fileFilter: function (req, file, cb) {
-        const fileExtension = path.extname(file.originalname).toLowerCase();
-        if (fileExtension !== '.pdf') {
-            return cb(new Error('Arquivo inválido. Por favor, envie um arquivo PDF.'));
-        }
-        cb(null, true);
-    }
-});
+const upload = multer({ storage: storage });
 
+async function uploadToGCP(file) {
+  const destination = `faturas/${file.originalname}`;
+  await bucket.bucket(bucketName).upload(file.path, {
+    destination: destination,
+  });
+  console.log(`Arquivo enviado para o bucket: ${destination}`);
+  return `https://storage.googleapis.com/${bucketName}/${destination}`;
+}
+
+// Rota para upload de faturas
+// Rota para upload de faturas
 router.post('/', upload.single('file'), async (req, res) => {
-    if (!req.file || req.file.mimetype !== 'application/pdf') {
-        return res.status(400).json({ error: 'Arquivo inválido. Por favor, envie um arquivo PDF.' });
+  try {
+    const file = req.file;
+
+    if (!file) {
+      return res.status(400).json({ error: 'Nenhum arquivo enviado' });
+    }
+
+    // Extrair dados do PDF
+    const dadosFatura = await pdfParser.extractData(file.path);
+    //console.log('Dados extraídos do PDF:', dadosFatura);
+
+    // Enviar o arquivo PDF para o bucket no GCP
+    const pdfUrl = await uploadToGCP(file);
+
+    // Verificar se o cliente existe com base no número do cliente extraído
+    if (!dadosFatura.numCliente) {
+      return res.status(400).json({ error: 'Número do cliente não encontrado no PDF' });
     }
     
-    try {
-        const filePath = req.file.path;
-        console.log(`Processando arquivo PDF: ${filePath}`);
-        
-        // Processa o PDF
-        const dadosPdf = await pdfParser(filePath);
-        
-        return res.status(200).json({
-            message: 'PDF processado com sucesso',
-            filePath,
-            dados: dadosPdf
-        });
-    } catch (error) {
-        console.error('Erro ao processar o PDF:', error.message);
-        return res.status(500).json({ error: 'Erro ao processar o PDF' });
+    const cliente = await Cliente.findOne({ where: { numeroCliente: dadosFatura.numCliente } });
+    if (!cliente) {
+      return res.status(404).json({ error: 'Cliente não encontrado' });
     }
+
+    // Criar nova fatura no banco de dados
+    const novaFatura = await Fatura.create({
+      clienteId: cliente.id,
+      numero_fatura: dadosFatura.numInstalacao,
+      valor_total: parseFloat(dadosFatura.totalPagar) || 0, // Certifica-se que valor é um número
+      referencia_mes: dadosFatura.mesReferencia,
+      vencimento: new Date(dadosFatura.vencimento), // Converte vencimento para um objeto Date
+      valor_a_pagar: parseFloat(dadosFatura.totalPagar) || 0, // Certifica-se que valor é um número
+      consumo_kwh: dadosFatura.consumo || 0, // Define um valor padrão para evitar null ou NaN
+      itens_faturados: {
+        energiaEletrica: dadosFatura.valoreFaturados.energiaEletrica,
+        energiaSCEESICMS: dadosFatura.valoreFaturados.energiaSCEESICMS,
+        energiaCompensadaGD: dadosFatura.valoreFaturados.energiaCompensadaGD,
+        contribuicaoIluminacao: dadosFatura.valoreFaturados.contribuicaoIluminacao
+      }
+    });
+    
+
+
+    console.log('Fatura criada com sucesso');
+
+    res.status(201).json({ message: 'Fatura criada com sucesso', fatura: novaFatura });
+  } catch (error) {
+    console.error('Erro ao fazer upload da fatura:', error);
+    res.status(500).json({ error: 'Erro ao fazer upload da fatura' });
+  }
 });
 
 module.exports = router;
