7561a49979441e6cceacc1041a6bb4c0bf738e66
luan-ligapi <luan@ligapi.com.br>
2024-10-20 22:25:07 -0300

    ajuste models

--- models/faturaDetalhes.js
+++ models/faturaDetalhes.js
@@ -1,42 +1,97 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-
   const FaturaDetalhes = sequelize.define('FaturaDetalhes', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    faturaId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Fatura', // Verifique se o nome da tabela de fatura está correto
+        key: 'id'
+      }
+    },
     energiaEletrica_valor: {
       type: DataTypes.DECIMAL,
-      allowNull: false
+      allowNull: true,
     },
     energiaEletrica_unidade: {
-      type: DataTypes.STRING,
-      allowNull: false
+      type: DataTypes.STRING(50),
+      allowNull: true,
     },
     energiaEletrica_precoUnit: {
       type: DataTypes.DECIMAL,
-      allowNull: false
+      allowNull: true,
     },
     energiaEletrica_quantidade: {
       type: DataTypes.DECIMAL,
-      allowNull: false
+      allowNull: true,
     },
     energiaEletrica_tarifaUnit: {
       type: DataTypes.DECIMAL,
-      allowNull: false
+      allowNull: true,
     },
     energiaSCEESICMS_valor: {
       type: DataTypes.DECIMAL,
-      allowNull: false
+      allowNull: true,
+    },
+    energiaSCEESICMS_unidade: {
+      type: DataTypes.STRING(50),
+      allowNull: true,
+    },
+    energiaSCEESICMS_precoUnit: {
+      type: DataTypes.DECIMAL,
+      allowNull: true,
+    },
+    energiaSCEESICMS_quantidade: {
+      type: DataTypes.DECIMAL,
+      allowNull: true,
+    },
+    energiaSCEESICMS_tarifaUnit: {
+      type: DataTypes.DECIMAL,
+      allowNull: true,
     },
     energiaCompensadaGD_valor: {
       type: DataTypes.DECIMAL,
-      allowNull: true
+      allowNull: true,
+    },
+    energiaCompensadaGD_unidade: {
+      type: DataTypes.STRING(50),
+      allowNull: true,
+    },
+    energiaCompensadaGD_precoUnit: {
+      type: DataTypes.DECIMAL,
+      allowNull: true,
     },
     energiaCompensadaGD_quantidade: {
       type: DataTypes.DECIMAL,
-      allowNull: true
-    }
+      allowNull: true,
+    },
+    energiaCompensadaGD_tarifaUnit: {
+      type: DataTypes.DECIMAL,
+      allowNull: true,
+    },
+    contribuicaoIluminacao: {
+      type: DataTypes.DECIMAL,
+      allowNull: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.literal('NOW()'),
+      allowNull: false,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.literal('NOW()'),
+      allowNull: false,
+    },
   }, {
     tableName: 'FaturaDetalhes',
-    schema: 'lumi', // Verifique se este schema é correto
+    schema: 'lumi', // Verifique se este schema está correto
     timestamps: true
   });
 
